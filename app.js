17ea7b41272acc08219683cecb6203f5c5f1984a
Vladimir <bobuzh@gmail.com>
2019-12-09 07:50:12 +0200

    modal window

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ $(window).scroll(function() {
       $('.menu').removeClass('shrink');
     }
   });
+
+  $('#myModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) 
+    var img = button.data('img') 
+    var client = button.data('client')
+    var category = button.data('category')
+    var modal = $(this)
+
+
+    console.log(modal.find('.modal-body img').attr('src'))
+    modal.find('.modal-body img').attr('src',"img/"+img+"-full.jpg")
+
+    modal.find('.client').text('Client: ' + client)
+    modal.find('.category').text('Category: ' + category)
+  })
